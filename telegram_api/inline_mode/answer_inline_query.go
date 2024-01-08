6290e98eca83ecec02e67e678b60b04149f48a2c
Alexander Sudalin <sudalin@MacBook-Air-Alexander.local>
2024-01-08 18:28:18 +0300

    fix: hot fix

--- telegram_api/inline_mode/answer_inline_query.go
+++ telegram_api/inline_mode/answer_inline_query.go
@@ -2,7 +2,7 @@ package inline
 
 type AnswerInlineQuery struct {
 	InlineQueryId string                   `json:"inline_query_id"`
-	Results       any                      `json:"results"`
+	Results       []any                    `json:"results"`
 	CacheTime     int32                    `json:"cache_time,omitempty"`
 	IsPersonal    bool                     `json:"is_personal,omitempty"`
 	NextOffset    string                   `json:"next_offset,omitempty"`
